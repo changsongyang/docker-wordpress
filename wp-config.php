fdd3985a116bb46f71531e87b3e10cb7f1f35c58
root <root@minux.home>
2017-01-23 14:10:06 -0500

    fix upload dir

--- wp-config.php
+++ wp-config.php
@@ -4,5 +4,5 @@ define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp/wp-content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp/wp-content');
 define( 'WP_PLUGIN_DIR', dirname(__FILE__) . '/wp/wp-content/plugins' );
 define( 'WP_PLUGIN_URL',  'http://' . $_SERVER['HTTP_HOST'] . '/wp/wp-content/plugins');
-define( 'UPLOADS',  'wp-content/uploads' );
+define( 'UPLOADS',  'wp/wp-content/uploads' );
 include( dirname(__FILE__) . '/wp/wp-config.php' );
