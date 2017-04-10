ef933cec5880e17b96fe071885531c643649e3e0
root <root@minux.home>
2017-04-10 02:35:24 -0400

    auto detect https

--- wp-config.php
+++ wp-config.php
@@ -1,11 +1,12 @@
 <?php
+define('WP_SITE_URI', ($_SERVER["HTTPS"]?"https://":"http://").$_SERVER["HTTP_HOST"]);
 
 //move wp-content to wp directory to make mountable
 define( 'WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp/wp-content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp/wp-content');
+define( 'WP_CONTENT_URL', WP_SITE_URI . '/wp/wp-content');
 
 define( 'WP_PLUGIN_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp/wp-content/plugins' );
-define( 'WP_PLUGIN_URL',  'http://' . $_SERVER['HTTP_HOST'] . '/wp/wp-content/plugins');
+define( 'WP_PLUGIN_URL',  WP_SITE_URI . '/wp/wp-content/plugins');
 
 define( 'UPLOADS',  'wp/wp-content/uploads' );
 
